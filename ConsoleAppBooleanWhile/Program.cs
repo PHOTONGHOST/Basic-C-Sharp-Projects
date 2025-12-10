843f88c904327b7ceb424766feebbf0a0a4622f7
PHOTONGHOST <calebcbeckwith97@gmail.com>
2025-12-10 18:26:05 -0500

    ConsoleAppDoWhileAssignment
    A simple console app program created using C# in Visual Studio 2022. Intended to show odd While and do while loops.

---
+++ ConsoleAppBooleanWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WhileDoWhileAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+  
+            // Boolean Comparison using a WHILE loop
+            int counter = 0;
+
+            while (counter < 5)
+            {
+                Console.WriteLine("While Loop: Counter is " + counter);
+
+                counter++;
+            }
+
+
+            Console.WriteLine();
+
+
+            // Boolean Comparison using a DO-WHILE loop
+
+            int number = 10;
+
+            // Note that the do-while loop will always run at least once
+            do
+            {
+                Console.WriteLine("Do-While Loop: Number is " + number);
+
+                number--;
+
+            } while (number > 7); // Condition, this will run again while the number is greater than 7
+
+
+            // The end of program
+            Console.WriteLine();
+            Console.WriteLine("Program complete. Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
