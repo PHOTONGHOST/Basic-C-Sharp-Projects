ad3adac2a91ad9a75e02967646e8ff7d46a5d1f2
PHOTONGHOST <calebcbeckwith97@gmail.com>
2026-01-23 22:16:10 -0500

    Datetime Assignment
    A simple assignment for practicing showing the current date and time, while allowing the user to see the date/time however many hours into the future they have entered.

---
+++ DatetimeAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will display the current date and time
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("Current date and time:");
+            Console.WriteLine(currentDateTime);
+
+            // Ask's the user for a number of hours
+            Console.WriteLine("\nEnter a number of hours:");
+
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            // This calculates the future date and time then adds the user-entered number of hours to the current time
+            DateTime futureDateTime = currentDateTime.AddHours(hours);
+
+            // This will display the future date and time
+            Console.WriteLine("\nThe date and time in " + hours + " hour(s) will be:");
+            Console.WriteLine(futureDateTime);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
